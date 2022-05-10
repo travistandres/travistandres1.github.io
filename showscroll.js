d4e5b34d3cfa2fc3e1aa8b8993602ab180783f2f
travistandres <79394353+travistandres@users.noreply.github.com>
2022-05-10 12:48:26 -0500

    Add files via upload

---
+++ showscroll.js
@@ -0,0 +1,61 @@
+// Detect request animation frame
+var scroll = window.requestAnimationFrame ||
+             // IE Fallback
+             function(callback){ window.setTimeout(callback, 1000/60)};
+var elementsToShow = document.querySelectorAll('.showscroll'); 
+
+function loop() {
+
+  elementsToShow.forEach(function (element) {
+    if (isElementInViewport(element)) {
+      element.classList.add('is-visible');
+    } else {
+      element.classList.remove('is-visible');
+    }
+  });
+
+  scroll(loop);
+}
+
+// Call the loop for the first time
+loop();
+function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return ( 
+    (rect.right <= (window.innerWidth || document.documentElement.clientWidth) && 
+    rect.bottom <= (window.innerWidth || document.documentElement.clientWidth) && 
+    rect.left <=   (window.innerWidth || document.documentElement.clientWidth) && 
+    rect.top <= (window.innerWidth || document.documentElement.clientWidth)
+  ));
+}
+
+
+  
+// create references to the modal...
+var modal = document.getElementById('myModal');
+// to all images -- note I'm using a class!
+var images = document.getElementsByClassName('myImages');
+// the image in the modal
+var modalImg = document.getElementById("img01");
+// and the caption in the modal
+var captionText = document.getElementById("caption");
+
+// Go through all of the images with our custom class
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  // and attach our click listener for this image.
+  img.onclick = function(evt) {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  }
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
